c13536f59e4f87fed39a18d865e7fe3042ccc274
Marko Stanojlovic <mstanojlovic@suse.com>
2018-03-02 16:57:10 +0100

    tour scraper finished

---
+++ timba_tours/tour/tour/spiders/spain.jl
@@ -0,0 +1,36 @@
+{"Charanga Habanera": "January 28, 2018 - Barcelona, Spain -\u00a0Calle Aribau SuIte 191 with Baby Lores"}
+{"Charanga Habanera": "February 4, 2018 - Malaga, Spain - Disco Sala Lafarandula"}
+{"Charanga Habanera": "Santiago de Compostela, Spain - Malatesta"}
+{"Charanga Habanera": "\u00a0Madrid, Spain - Teatro Gran Maestre"}
+{"Charanga Habanera": "February 10, 2018 - Valencia, Spain - Sala Fussion"}
+{"Gente D' Zona": "July 22, 2016 - Murcia, Spain "}
+{"Gente D' Zona": "July 23, 2016 - Tarragona, Spain "}
+{"Gente D' Zona": "July 24, 2016 - Valencia, Spain "}
+{"Gente D' Zona": "July 29, 2016 - Legan\u00e9s, Spain "}
+{"Gente D' Zona": "July 30, 2016 - Marbella, Spain "}
+{"Gente D' Zona": "July 31, 2016 - Barcelona, Spain "}
+{"Jacob Forever": "February 4, 2018 - Madrid, Spain"}
+{"Jacob Forever": "February 10, 2018 - Valencia, Spain"}
+{"Jacob Forever": "February 11, 2018 - Barcelona, Spain"}
+{"Jovenes Clasicos del Son": "July 4, 2017 - Madrid, Spain - Sala Galileo"}
+{"Jovenes Clasicos del Son": "July 16, 2017 - Tarragona, Spain - La Isla del Mojito"}
+{"Jovenes Clasicos del Son": "July 20, 2017 - \u00c1guilas (Murcia), Spain - Auditorio de \u00c1guilas"}
+{"Jovenes Clasicos del Son": "August 8, 2017 - Madrid, Spain - Sala Galileo Galilei Buy tickets"}
+{"Laritza Bacallao": "July 26, 2015 - Matar\u00f3, Spain - Salsa Republic Mataro "}
+{"Mayimbe, Barbaro Fines y su Orquesta": "February 16, 2018 - Seville, Spain - Chaney Discotek"}
+{"Mayimbe, Barbaro Fines y su Orquesta": "February 25, 2018 - Barcelona, Spain - El New Cel"}
+{"Pachito Alonso & Sus Kini Kini": "April 28, 2013 - Madrid, Spain"}
+{"Rev\u00e9, Orquesta": "February 11, 2018 - Pamplona, Spain - Zentral"}
+{"Rev\u00e9, Orquesta": "February 22, 2018 - Madrid, Spain - Cool Club"}
+{"Rev\u00e9, Orquesta": "February 23, 2018 - Alicante, Spain - Salsa Club de Alicante"}
+{"Rev\u00e9, Orquesta": "February 24, 2018 - Malaga, Spain - Hotel Alay - CC en Andalucia"}
+{"Rev\u00e9, Orquesta": "February 25, 2018 - Barcelona, Spain - New Cel with B\u00e1rbaro Fines y su Mayimbe"}
+{"Rev\u00e9, Orquesta": "March 2, 2018 -Mallorca, Spain"}
+{"Pupy y Los Que Son Son": "March 16, 2018 - Madrid, Spain"}
+{"Pupy y Los Que Son Son": "March 21, 2018 - Santiago de Compostela, Spain - Sala Malatesta"}
+{"Pupy y Los Que Son Son": "March 23, 2018 - Bilbao, Spain"}
+{"Pupy y Los Que Son Son": "March 24, 2018 - M\u00e1laga, Spain - Disco Lafarandula"}
+{"Pupy y Los Que Son Son": "April 13, 2018 - Malaga, Spain"}
+{"Pupy y Los Que Son Son": "April 14, 2018 - Valencia, Spain"}
+{"Pupy y Los Que Son Son": "April 15, 2018 - Barcelona, Spain"}
+{"Timbalive": "January 28 - February 17, 2015 - Switzerland, Germany, Spain, Russia"}
--- timba_tours/tour/tour/spiders/tours.py
+++ timba_tours/tour/tour/spiders/tours.py
@@ -11,15 +11,20 @@
 # scrapy shell 'http://www.timba.com/tours'
 # response.xpath("(//table[@class='striped_table']//a//@href)")
 # response.xpath("//div[@class='pagination']//a[text()='Next']//@href").extract_first()
-
+# remove_tags(response.xpath("(//div[@id='tour_div']//p/strong)")[7].extract())
+# selecting name of the band:
+# v1: response.xpath("(//h3[@class='box_header']/text())")[-1].extract()
+# v2: response.xpath("(//div[@id='main_page']//h3[@class='box_header']/text())").extract_first()
 import scrapy
+import json
+from w3lib.html import remove_tags
 
 class TimbaTourSpider(scrapy.Spider):
     name = "timbatours"
 
     custom_settings = {
         'ROBOTSTXT_OBEY': 'False',
-        'DOWNLOAD_DELAY': '5.0',
+        'DOWNLOAD_DELAY': '0.7',
         'RANDOMIZE_DOWNLOAD_DELAY': 'True'
     }
 
@@ -41,12 +46,9 @@ class TimbaTourSpider(scrapy.Spider):
 class GetTours(scrapy.Spider):
     name = "gettours"
 
-    import json
-    from w3lib.html import remove_tags
-
     custom_settings = {
         'ROBOTSTXT_OBEY': 'False',
-        'DOWNLOAD_DELAY': '5.0',
+        'DOWNLOAD_DELAY': '0.2',
         'RANDOMIZE_DOWNLOAD_DELAY': 'True'
     }
 
@@ -57,8 +59,13 @@ class GetTours(scrapy.Spider):
     start_urls = links
 
     def parse(self, response):
-        for concert in response.xpath("(//div[@id='tour_div']//p/strong/text())"):
-            print concert.extract()
+        search_filter = getattr(self, 'filter', None)
+        for concert in response.xpath("(//div[@id='tour_div']//p/strong)"):
+            info = remove_tags(concert.extract())
+            band_name = response.xpath("(//div[@id='main_page']//h3[@class='box_header']/text())").extract_first()
+            if search_filter.upper() in info.upper():
+                yield { band_name : info }
+
 
 
 
