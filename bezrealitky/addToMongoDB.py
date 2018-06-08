48afd614be24dfd6e99202e2e4cf45146a1d012f
Marko Stanojlovic <stanojlovic.marko@gmail.com>
2018-06-08 12:49:08 +0200

    typo on pymongo

--- bezrealitky/addToMongoDB.py
+++ bezrealitky/addToMongoDB.py
@@ -21,7 +21,7 @@ for key in keys:
 pprint(data)
 
 # connecting to a mongodb
-import pymongos
+import pymongo
 from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.test_database # get a db 
